1c219f9131d06801f409a425e03da94e8fbfce32
Jakub Turlinski <jakubkt@gmail.com>
2023-03-22 19:54:01 +0100

    Adding OnClickListeners for buttons in CalibrationActivity.kt. Creating template to change calibration values in root_preferences.xml and CalibrationActivity.kt.

--- app/src/main/java/edu/jakubkt/soundpressurelevelmeter/CalibrationActivity.kt
+++ app/src/main/java/edu/jakubkt/soundpressurelevelmeter/CalibrationActivity.kt
@@ -33,7 +33,7 @@ class CalibrationActivity : AppCompatActivity(), AudioBufferProcessing {
     private var updateUI: Boolean = true
 
     //TODO retrieve calibration values from Settings Activity
-    private val octaveBandCalibrationValues = IntArray(8) { 0 }
+    private val octaveBandsCalibrationValues = IntArray(8) { 0 }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -95,49 +95,126 @@ class CalibrationActivity : AppCompatActivity(), AudioBufferProcessing {
     }
 
     override fun processAudioBuffer(audioBuffer: ShortArray?) {
-        //TODO Finish implementing
+        //TODO For every octave band calculate Leq, then apply calibration correction
         if(updateUI) {
             updateUI = false
 
-            val linstValueOctaveBand125Hz = random.nextDouble() * RANDOM_MAX_RANGE
-            val linstValueOctaveBand250Hz = random.nextDouble() * RANDOM_MAX_RANGE
-            val linstValueOctaveBand500Hz = random.nextDouble() * RANDOM_MAX_RANGE
-            val linstValueOctaveBand1000Hz = random.nextDouble() * RANDOM_MAX_RANGE
-            val linstValueOctaveBand2000Hz = random.nextDouble() * RANDOM_MAX_RANGE
-            val linstValueOctaveBand4000Hz = random.nextDouble() * RANDOM_MAX_RANGE
-            val linstValueOctaveBand8000Hz = random.nextDouble() * RANDOM_MAX_RANGE
-            val linstValueOctaveBand16000Hz = random.nextDouble() * RANDOM_MAX_RANGE
+            val linstValueOctaveBand125Hz = 50.0 //random.nextDouble() * RANDOM_MAX_RANGE
+            val linstValueOctaveBand250Hz = 50.0 //random.nextDouble() * RANDOM_MAX_RANGE
+            val linstValueOctaveBand500Hz = 50.0 //random.nextDouble() * RANDOM_MAX_RANGE
+            val linstValueOctaveBand1000Hz = 50.0 //random.nextDouble() * RANDOM_MAX_RANGE
+            val linstValueOctaveBand2000Hz = 50.0 //random.nextDouble() * RANDOM_MAX_RANGE
+            val linstValueOctaveBand4000Hz = 50.0 //random.nextDouble() * RANDOM_MAX_RANGE
+            val linstValueOctaveBand8000Hz = 50.0 //random.nextDouble() * RANDOM_MAX_RANGE
+            val linstValueOctaveBand16000Hz = 50.0 //random.nextDouble() * RANDOM_MAX_RANGE
 
             runOnUiThread {
                 // Rounding floating-point number to 1 decimal place
                 val df = DecimalFormat("#.#")
                 df.roundingMode = RoundingMode.HALF_UP
 
-                binding.linstValueOctaveBand125Hz.text = df.format(linstValueOctaveBand125Hz + octaveBandCalibrationValues[0])
-                binding.linstValueOctaveBand250Hz.text = df.format(linstValueOctaveBand250Hz)
-                binding.linstValueOctaveBand500Hz.text = df.format(linstValueOctaveBand500Hz)
-                binding.linstValueOctaveBand1000Hz.text = df.format(linstValueOctaveBand1000Hz)
-                binding.linstValueOctaveBand2000Hz.text = df.format(linstValueOctaveBand2000Hz)
-                binding.linstValueOctaveBand4000Hz.text = df.format(linstValueOctaveBand4000Hz)
-                binding.linstValueOctaveBand8000Hz.text = df.format(linstValueOctaveBand8000Hz)
-                binding.linstValueOctaveBand16000Hz.text = df.format(linstValueOctaveBand16000Hz)
+                binding.linstValueOctaveBand125Hz.text = df.format(linstValueOctaveBand125Hz + octaveBandsCalibrationValues[0])
+                binding.linstValueOctaveBand250Hz.text = df.format(linstValueOctaveBand250Hz + octaveBandsCalibrationValues[1])
+                binding.linstValueOctaveBand500Hz.text = df.format(linstValueOctaveBand500Hz + octaveBandsCalibrationValues[2])
+                binding.linstValueOctaveBand1000Hz.text = df.format(linstValueOctaveBand1000Hz + octaveBandsCalibrationValues[3])
+                binding.linstValueOctaveBand2000Hz.text = df.format(linstValueOctaveBand2000Hz + octaveBandsCalibrationValues[4])
+                binding.linstValueOctaveBand4000Hz.text = df.format(linstValueOctaveBand4000Hz + octaveBandsCalibrationValues[5])
+                binding.linstValueOctaveBand8000Hz.text = df.format(linstValueOctaveBand8000Hz + octaveBandsCalibrationValues[6])
+                binding.linstValueOctaveBand16000Hz.text = df.format(linstValueOctaveBand16000Hz + octaveBandsCalibrationValues[7])
 
-                binding.calibrationValueOctaveBand125Hz.text = octaveBandCalibrationValues[0].toString()
+                binding.calibrationValueOctaveBand125Hz.text = octaveBandsCalibrationValues[0].toString()
+                binding.progressbarOctaveBand125Hz.progress = (linstValueOctaveBand125Hz).toInt() + octaveBandsCalibrationValues[0]
 
-                binding.progressbarOctaveBand125Hz.progress = (linstValueOctaveBand125Hz).toInt() + octaveBandCalibrationValues[0]
+                binding.calibrationValueOctaveBand250Hz.text = octaveBandsCalibrationValues[1].toString()
+                binding.progressbarOctaveBand250Hz.progress = (linstValueOctaveBand250Hz).toInt() + octaveBandsCalibrationValues[1]
+
+                binding.calibrationValueOctaveBand500Hz.text = octaveBandsCalibrationValues[2].toString()
+                binding.progressbarOctaveBand500Hz.progress = (linstValueOctaveBand500Hz).toInt() + octaveBandsCalibrationValues[2]
+
+                binding.calibrationValueOctaveBand1000Hz.text = octaveBandsCalibrationValues[3].toString()
+                binding.progressbarOctaveBand1000Hz.progress = (linstValueOctaveBand1000Hz).toInt() + octaveBandsCalibrationValues[3]
+
+                binding.calibrationValueOctaveBand2000Hz.text = octaveBandsCalibrationValues[4].toString()
+                binding.progressbarOctaveBand2000Hz.progress = (linstValueOctaveBand2000Hz).toInt() + octaveBandsCalibrationValues[4]
+
+                binding.calibrationValueOctaveBand4000Hz.text = octaveBandsCalibrationValues[5].toString()
+                binding.progressbarOctaveBand4000Hz.progress = (linstValueOctaveBand4000Hz).toInt() + octaveBandsCalibrationValues[5]
+
+                binding.calibrationValueOctaveBand8000Hz.text = octaveBandsCalibrationValues[6].toString()
+                binding.progressbarOctaveBand8000Hz.progress = (linstValueOctaveBand8000Hz).toInt() + octaveBandsCalibrationValues[6]
+
+                binding.calibrationValueOctaveBand16000Hz.text = octaveBandsCalibrationValues[7].toString()
+                binding.progressbarOctaveBand16000Hz.progress = (linstValueOctaveBand16000Hz).toInt() + octaveBandsCalibrationValues[7]
+
                 updateUI = true
             }
         }
     }
 
-    fun setOnClickListeners() {
+    private fun setOnClickListeners() {
         //TODO set button listeners for every button in every octave band
         binding.buttonUpOctaveBand125Hz.setOnClickListener {
-            octaveBandCalibrationValues[0]++
+            octaveBandsCalibrationValues[0]++
         }
 
         binding.buttonDownOctaveBand125Hz.setOnClickListener {
-            octaveBandCalibrationValues[0]--
+            octaveBandsCalibrationValues[0]--
+        }
+
+        binding.buttonUpOctaveBand250Hz.setOnClickListener {
+            octaveBandsCalibrationValues[1]++
+        }
+
+        binding.buttonDownOctaveBand250Hz.setOnClickListener {
+            octaveBandsCalibrationValues[1]--
+        }
+
+        binding.buttonUpOctaveBand500Hz.setOnClickListener {
+            octaveBandsCalibrationValues[2]++
+        }
+
+        binding.buttonDownOctaveBand500Hz.setOnClickListener {
+            octaveBandsCalibrationValues[2]--
+        }
+
+        binding.buttonUpOctaveBand1000Hz.setOnClickListener {
+            octaveBandsCalibrationValues[3]++
+        }
+
+        binding.buttonDownOctaveBand1000Hz.setOnClickListener {
+            octaveBandsCalibrationValues[3]--
+        }
+
+        binding.buttonUpOctaveBand2000Hz.setOnClickListener {
+            octaveBandsCalibrationValues[4]++
+        }
+
+        binding.buttonDownOctaveBand2000Hz.setOnClickListener {
+            octaveBandsCalibrationValues[4]--
+        }
+
+        binding.buttonUpOctaveBand4000Hz.setOnClickListener {
+            octaveBandsCalibrationValues[5]++
+        }
+
+        binding.buttonDownOctaveBand4000Hz.setOnClickListener {
+            octaveBandsCalibrationValues[5]--
+        }
+
+        binding.buttonUpOctaveBand8000Hz.setOnClickListener {
+            octaveBandsCalibrationValues[6]++
+        }
+
+        binding.buttonDownOctaveBand8000Hz.setOnClickListener {
+            octaveBandsCalibrationValues[6]--
+        }
+
+        binding.buttonUpOctaveBand16000Hz.setOnClickListener {
+            octaveBandsCalibrationValues[7]++
+        }
+
+        binding.buttonDownOctaveBand16000Hz.setOnClickListener {
+            octaveBandsCalibrationValues[7]--
         }
     }
 }
